a49302f05bad49ea4cd22ed8efcbcc76b7287f05
Flammie Pirinen <flammie@iki.fi>
2014-05-25 10:44:27 +0100

    handle blacklisting top-level dirs

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-for f in $HOME/.* ; do
-    dotf=${f#$HOME/.}
-    if test -d ${f} ; then
-        echo Skipping directory $f
+for d in $HOME/.*  ; do
+    brek=false
+    while read b ; do
+        if test $d = $HOME/$b ; then
+            brek=true
+        fi
+    done < dotfileblacklist
+    if $brek ; then
         continue
     fi
-    if ! diff -Nu ${dotf} ${f} ; then
-        echo Above diff will be applied by cp overwrite?
-        select a in yes no ; do
-            if test x$a = xyes ; then
-                cp -v $f $dotf
-            elif test x$a = xno ; then
-                echo Skipped
-            else
-                echo Unknown $a
-            fi
-            break
-        done
-    fi
+    for f in $(find $d -type f ) ; do
+        dotf=${f#$HOME/.}
+        if ! diff -Nu "${dotf}" "${f}" ; then
+            echo Above diff will be applied by cp overwrite?
+            select a in yes no ; do
+                if test x$a = xyes ; then
+                    cp -v "$f" "$dotf"
+                elif test x$a = xno ; then
+                    echo Skipped
+                else
+                    echo Unknown $a
+                fi
+                break
+            done
+        fi
+    done
 done
