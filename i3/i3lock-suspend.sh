4666f25d0e4ed64ea5f4033e0d72ce2e38515445
Flammie Pirinen <flammie@iki.fi>
2015-12-15 20:20:35 +0000

    only image magick used no scrot

--- i3/i3lock-suspend.sh
+++ i3/i3lock-suspend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-scrot /tmp/i3locked.png
-mogrify -scale 10% -scale 1000% /tmp/i3locked.png
-i3lock -i /tmp/i3locked.png; sleep 300 && $HOME/.i3/i3-sensible-suspend
+import -depth 8 -window root /tmp/i3locked.png
+mogrify -scale 10% -scale 1000% -gravity center -fill  white -pointsize 128 -annotate 50%x50%-320 LOCKED! /tmp/i3locked.png 
+i3lock -i /tmp/i3locked.png
