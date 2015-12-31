c8feb0221902a00f6facaaa804e989bb8f8e2736
Flammie Pirinen <flammie@iki.fi>
2015-12-31 19:30:46 +0000

    random bg from picture dir (risky!)

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -3,4 +3,4 @@ xautolock -detectsleep -locker $HOME/.i3/i3lock-suspend.sh &
 synclient VertScrollDelta=-112
 synclient TapButton1=1
 synclient TapButton2=3
-
+feh --bg-max --randomize $(xdg-user-dir PICTURES)/
