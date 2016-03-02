c7e3a90da72ba3f037bfa044ba33bed1a16b279a
Flammie Pirinen <flammie@iki.fi>
2016-03-02 17:53:38 +0100

    screen layouts

---
+++ screenlayout/rotate-left.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --primary --mode 1920x1200 --pos 1200x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output VGA1 --mode 1920x1200 --pos 0x0 --rotate left
