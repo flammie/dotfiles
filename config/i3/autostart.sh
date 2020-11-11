9fb8772ffa6eeb06afb718fa845b0f8f8692eea3
Flammie Pirinen <flammie@iki.fi>
2020-11-11 05:40:35 +0100

    more fixups to avoid touchpad fu
    it's just that bad on most linux drivers...

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 xss-lock -l -- ~/.config/i3/i3lock.sh &
-#synclient VertScrollDelta=-112
-synclient TapButton1=1
-synclient TapButton2=3
-synclient MaxTapMove=10
+synclient VertScrollDelta=-72
+synclient TapButton1=0
+synclient TapButton2=1
+synclient TapButton3=3
 #bash $HOME/.screenlayout/rotate-left.sh
 feh --bg-max --randomize $(xdg-user-dir PICTURES)/
 pacmd < compressor.pacmd
