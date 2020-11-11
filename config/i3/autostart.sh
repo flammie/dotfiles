ad094d50f43b3dadaf158cff07949a4876bea2a5
Flammie Pirinen <flammie@iki.fi>
2020-11-11 04:53:48 +0100

    more reorg and new synaptics

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 xss-lock -l -- ~/.config/i3/i3lock.sh &
 #synclient VertScrollDelta=-112
-#synclient TapButton1=1
-#synclient TapButton2=3
+synclient TapButton1=1
+synclient TapButton2=3
+synclient MaxTapMove=10
 #bash $HOME/.screenlayout/rotate-left.sh
 feh --bg-max --randomize $(xdg-user-dir PICTURES)/
 pacmd < compressor.pacmd
