5f7a4671ea764afd50c8c5919479f4d9616795ba
Flammie Pirinen <flammie@iki.fi>
2021-08-04 12:43:47 +0200

    group autostarts

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+nm-applet &
+pasystray &
+skypeforlinux &
+telegram-desktop &
+# gnome-settings-daemon &
+# unity-settings-daemon &
 xss-lock -l -- ~/.config/i3/i3lock.sh &
 synclient VertScrollDelta=-72
 synclient TapButton1=0
 synclient TapButton2=1
 synclient TapButton3=3
 #bash $HOME/.screenlayout/rotate-left.sh
-feh --bg-max --randomize $(xdg-user-dir PICTURES)/
-pacmd < compressor.pacmd
+feh --bg-max --randomize "$(xdg-user-dir PICTURES)/"
 redshift-gtk &
+gxkb &
+steam-client &
