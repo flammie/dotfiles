a6fea0a155080c402af695b5b21bd6a38955e982
Flammie Pirinen <flammie@iki.fi>
2022-01-20 17:12:10 +0100

    more synclient, teams, nice skype

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 nm-applet &
 pasystray &
-skypeforlinux &
+nice skypeforlinux &
 telegram-desktop &
+teams &
 # gnome-settings-daemon &
 # unity-settings-daemon &
-xss-lock -l -- ~/.config/i3/i3lock.sh &
+# xss-lock -l -- ~/.config/i3/i3lock.sh &
+xscreensaver &
 synclient VertScrollDelta=-72
 synclient TapButton1=0
 synclient TapButton2=1
 synclient TapButton3=3
+synclient MaxSpeed=3
+synclient AccelFactor=0.110
 #bash $HOME/.screenlayout/rotate-left.sh
 feh --bg-max --randomize "$(xdg-user-dir PICTURES)/"
 redshift-gtk &
 gxkb &
-steam-client &
