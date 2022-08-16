4a0bc103ca0cf94094b539fe7970d0d6beee564d
Flammie A Pirinen <flammie@iki.fi>
2022-08-16 17:11:40 +0200

    slightly updated batterymon stuff for cleaner logs
    inspired by https://github.com/rhz/sd-battery-notification

--- bin/battery-monitor.bash
+++ bin/battery-monitor.bash
@@ -9,11 +9,20 @@ battery_state=$(acpi | grep 'Battery' | sed 's/Battery\s[0-9]*: //' | sed 's/, [
 #battery_remaining=$(acpi | grep -oh '[0-9:]* remaining' | sed 's/:\w\w remaining$/ Minutes/'  | sed 's/00://' | sed 's/:/h /')
 
 if test "$battery_state" = "Discharging" ; then
-    if test "$battery_level" -le 5 ; then
+    if test "$battery_level" -le 5 -a ! -e warned-2 ; then
         notify-send '⚡⚡⚡ Virta loppu!' 'Sammutetaan ihan just...' \
             -u critical
-    elif test "$battery_level" -le 10 ; then
+        touch warned-2
+    elif test "$battery_level" -le 10 -a ! -e warned-1 ; then
         notify-send '⚠⚡Virta loppuu!' '🗲🔋🔌Patteri alkaa olla lopussa!' \
             -u normal
+        touch warned-1
+    fi
+else
+    if test -e warned-1 ; then
+        rm warned-1
+    fi
+    if test -e warned-2 ; then
+        rm warned-2
     fi
 fi
--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-nm-applet &
-pasystray &
-nice skypeforlinux &
+
+# handled by dex?
+#nm-applet &
+# pasystray & # handled by dex?
+#nice skypeforlinux &
 telegram-desktop &
-teams &
+#teams &
 # gnome-settings-daemon &
 # unity-settings-daemon &
 # xss-lock -l -- ~/.config/i3/i3lock.sh &
 # xscreensaver &
-xss-lock -n /usr/libexec/xsecurelock/dimmer -l -- xsecurelock &
+# xss-lock -n /usr/libexec/xsecurelock/dimmer -l -- xsecurelock &
+xidlehook --not-when-fullscreen --not-when-audio \
+    --timer 60 ̈́'/usr/libexec/xsecurelock/dimmer' ''\
+    --timer 10 'xsecurelock' ''\
+    --timer 900 'systemctl suspend' '' &
 #env XSECURELOCK_SAVER=saver_xscreensaver xsecurelock
 synclient VertScrollDelta=-72
 synclient TapButton1=0
@@ -17,10 +23,11 @@ synclient TapButton3=3
 synclient MaxSpeed=3
 synclient AccelFactor=0.110
 #bash $HOME/.screenlayout/rotate-left.sh
-feh --bg-max --randomize "$(xdg-user-dir PICTURES)/"
+feh --bg-max --randomize "$(xdg-user-dir PICTURES)/" &
 redshift-gtk &
 gxkb &
 setxkbmap -layout fi,ru,no -variant ,phonetic, -option grp:shifts_toggle
 # xscreensaver-suspend.bash &
-xset s 60 5
-xset dpms 100 600 1200
+#xset s 60 5
+#xset dpms 100 600 1200
+# maybe xidlehook does it^^
