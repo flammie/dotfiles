f0bccd53ae577eb12b2ce3f0642da9773ad0a7fb
Flammie A Pirinen <flammie@iki.fi>
2025-01-29 14:44:13 +0100

    signal and proton mails

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+pulseaudio --check || pulseaudio -D
+# fixing bugs https://bugs.chromium.org/p/chromium/issues/detail?id=370022
+# https://github.com/google/xsecurelock/issues/24
+# https://github.com/google/xsecurelock/issues/97
+# xcompmgr -c &
+picom --backend glx --fade-exclude 'class_g = "xsecurelock"' &
+# picom --backend glx --paint-on-overlay
 # handled by dex?
 #nm-applet &
 # pasystray & # handled by dex?
 #nice skypeforlinux &
 telegram-desktop &
+signal-desktop --start-in-tray &
 #teams &
 # gnome-settings-daemon &
 # unity-settings-daemon &
 # xss-lock -l -- ~/.config/i3/i3lock.sh &
 # xscreensaver &
 # xss-lock -n /usr/libexec/xsecurelock/dimmer -l -- xsecurelock &
-xidlehook --not-when-fullscreen --not-when-audio \
-    --timer 60 ̈́'/usr/libexec/xsecurelock/dimmer' ''\
-    --timer 10 'xsecurelock' ''\
+PATH=/usr/libexec/xsecurelock/:$PATH xidlehook --not-when-fullscreen \
+    --timer 93 ̈́'/usr/libexec/xsecurelock/dimmer' ''\
+    --timer 12 'xsecurelock' ''\
     --timer 900 'systemctl suspend' '' &
 #env XSECURELOCK_SAVER=saver_xscreensaver xsecurelock
 synclient VertScrollDelta=-72
@@ -28,6 +36,9 @@ redshift-gtk &
 gxkb &
 setxkbmap -layout fi,ru,no -variant ,phonetic, -option grp:shifts_toggle
 # xscreensaver-suspend.bash &
-#xset s 60 5
-#xset dpms 100 600 1200
+# xset s 60 5
+# xset dpms 100 600 1200
 # maybe xidlehook does it^^
+#
+# just run proton mail bridge in detached screen (probs asks for gpg pass)
+screen -S protonmail -dm proton-mail-bridge --cli
